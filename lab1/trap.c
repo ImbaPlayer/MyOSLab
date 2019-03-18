2dc75fa53012c458f62151df5624c887ac764db0
ImbaPlayer <duangl16@mails.tsinghua.edu.cn>
2019-03-18 22:17:04 +0800

    fix bug of trap.c

--- lab1/trap.c
+++ lab1/trap.c
@@ -52,7 +52,7 @@ idt_init(void) {
         SETGATE(idt[i], 0, GD_KTEXT, __vectors[i],DPL_KERNEL);
     }
     SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
-    lidt(idt_pd);
+    lidt(&idt_pd);
 }
 
 static const char *
